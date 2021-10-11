a6b187d0dfe98f31eb3de22ee122752c64b4cd16
Guilherme <drumond.guilherme@hotmail.com>
2021-10-11 18:02:51 -0300

    Feature: List Users, Graphs and Tables

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,90 @@
-import { Header } from "../components/Header/header";
+import { Header } from "../components/Header"
+import { Flex, SimpleGrid, Box, Text, theme } from "@chakra-ui/react"
+import dynamic from 'next/dynamic'
+import { SideBar } from "../components/Sidebar"
+import { ApexOptions } from "apexcharts"
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+})
+
+const options: ApexOptions = {
+  chart:{
+    toolbar: {
+      show: false,
+    },
+    zoom:{
+      enabled: false,      
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid:{
+    show: false,
+  },
+  dataLabels:{
+    enabled: false,
+  },
+  tooltip:{
+    enabled: false,
+  },
+  xaxis:{
+    type: 'datetime',
+    axisBorder:{
+      color: theme.colors.gray[600]
+    },
+    axisTicks:{
+      color: theme.colors.gray[600]
+    },
+    categories:[
+      '2021-10-11T00:00:00.000Z',
+      '2021-10-12T00:00:00.000Z',
+      '2021-10-13T00:00:00.000Z',
+      '2021-10-14T00:00:00.000Z',
+      '2021-10-15T00:00:00.000Z',
+      '2021-10-16T00:00:00.000Z',
+      '2021-10-17T00:00:00.000Z',
+    ]    
+  },
+  fill:{
+    opacity: 0.3,
+    type:'gradient',
+    gradient:{
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3 
+    }
+  }
+};
+const series = [
+  { name: 'series1', data: [ 31,120, 222, 110, 10,12,400 ]}
+]
 
 export default function Dashboard(){
-  return(
-    <Header></Header>
-  )
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <SideBar />
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <Box
+            bg="gray.800"
+            borderRadius={8}
+            p="8"
+            pb="4"
+          >
+            <Text fontSize="lg" mb="4"> Inscritos da Semana</Text>
+            <Chart options={options} series={series} type="area" height={160}/>
+          </Box>
+          <Box
+            bg="gray.800"
+            borderRadius={8}
+            p="8"
+            pb="4"
+          >
+            <Text fontSize="lg" mb="4"> Taxa de Abertura</Text>
+            <Chart options={options} series={series} type="area" height={160}/>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  );
 }
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Th, Thead, Tr, Td, Text} from '@chakra-ui/react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { Header } from "../../components/Header"
+import { SideBar } from "../../components/Sidebar"
+
+export default function UserList(){
+  return(
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1460} mx="auto" px="6">
+        <SideBar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal"> Usuários </Heading>
+          <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+              >
+
+              Criar Novo
+            </Button>
+          </Flex>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink"/>
+                </Th>
+                <Th>Usuarios</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"/>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold"> Guilherme Fernandes Drumond </Text>
+                    <Text fontSize="sm" color="gray.300"> dev.drumond@gmail.com </Text>
+                  </Box>
+                </Td>
+                <Td>
+                  21 de Abril, 2021
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >Editar</Button>   
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
